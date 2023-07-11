e9614e5636c8de88a535a048ffc2e41cb58f586d
Ilya Ovchelupov <iovchelupoff@live.com>
2023-07-11 02:40:43 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
+import Version from './components/Version';
+
 function App() {
+
+  var title = "Project 'Cloud-Auto-update' v.1.0.0";
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <Version title={title} />
       </header>
     </div>
   );
