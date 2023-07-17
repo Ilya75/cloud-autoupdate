147a856c0a4c135a22cc365866914f28b999fd97
Ilya Ovchelupov <iovchelupoff@live.com>
2023-07-17 00:39:52 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -4,13 +4,16 @@ import Version from './components/Version';
 
 function App() {
 
-  var title = "Project 'Cloud-Auto-update' v.1.0.0";
+  var title = "Project 'Cloud-Autoupdate' v.0.1";
 
   return (
     <div className="App">
       <header className="App-header">
        <Version title={title} />
       </header>
+      <footer>
+        <span>Ilya Ovchelupov, 2023</span>
+      </footer>
     </div>
   );
 }
